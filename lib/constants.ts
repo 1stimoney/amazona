e8f060f4c7d9d4d403fd72655e52508be19ee00c
1stimoney <daudasheriff020@gmail.com>
2025-02-01 21:22:46 +0100

    initial commit

---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+export const SENDER_NAME = process.env.SENDER_NAME || 'support'
+export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'onboarding@resend.dev'
+export const SITE_URL = process.env.SITE_URL || 'http://localhost:3000'
+
+export const USER_ROLES = ['Admin', 'User']
+export const COLORS = ['Gold', 'Green', 'Red']
+export const THEMES = ['Light', 'Dark', 'System']
